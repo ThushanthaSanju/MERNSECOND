282b58f78c7274dc645831fb3eae9852048bcb34
Thushantha Sanju <thushanthasanju8@gmail.com>
2021-07-15 13:33:01 +0530

    First Commit

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+import { getPost, createPost } from '../controllers/posts.js'
+
+router.get('/', getPost);
+router.post('/', createPost)
+
+export default router;
